3a172f339301b52c12e03fc786a17119afc2480c
Mariano Fuentes <hardhopeid@gmail.com>
2023-10-04 15:45:23 -0500

    ¿Qué es el Hoisting?

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+/* var nameOfDog; // undefined
+
+console.log(nameOfDog);
+var nameOfDog = 'Elmo';
+console.log(nameOfDog); */
+
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${perrito}`);
+}
+
+var perrito = 'Teo';
