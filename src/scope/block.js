5dfa910437db92a40f5583593d64ec547684e2bc
Mariano Fuentes <hardhopeid@gmail.com>
2023-10-04 07:39:00 -0500

    Block Scope

---
+++ src/scope/block.js
@@ -0,0 +1,12 @@
+function fruits() {
+    if (true) {
+        var fruit1 = 'Apple'; //Funtion Scope
+        let fruit2 = 'Kiwi'; //Block Scope
+        const fruit3 = 'Banana'; // Block Scope
+        console.log (fruit2);
+        console.log (fruit3);
+    }
+    console.log (fruit1);
+}
+
+fruits();
