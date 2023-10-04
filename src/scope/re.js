01c06f8cfe16f7944a4300d774e73e69062f9ef5
Mariano Fuentes <hardhopeid@gmail.com>
2023-10-04 07:52:11 -0500

    Reasignación y redeclaración

---
+++ src/scope/re.js
@@ -0,0 +1,30 @@
+var firstName; //Undefined
+firstName = 'Oscar';
+console.log (firstName);
+
+var lastName = "David"; // Declarar / Asignar
+lastName = "Ana"; // Reasignar
+console.log (lastName);
+
+var secondName = 'David'; // Declarando / Asignando
+var secondName = 'Ana'; // Reasignado
+console.log (secondName);
+
+// let
+let fruit = 'Apple' // Declarar y Asignar
+fruit = 'kiwi'; //Reasignar
+console.log (fruit);
+
+let fruit = 'Banana';
+console.log (fruit);
+
+// const
+
+const animal = 'dog'; //Declarar y Asigna
+// animal = 'cat'; //Reasignado
+const animal = 'Snake';
+console.log (animal)
+
+const vehicles = [];
+vehicles.push("Carrito");
+console.log(vehicles);
